849fabb4dda201c5acad52db90326b2413d563be
Allen Woods <allen.h.woods@gmail.com>
2018-12-04 21:17:18 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	output_string = ""
+	array.each_with_index do |item, index|
+		"one, two, and three"
+		comma =				(array.length > 2 && index != array.length - 1) ? "," : ""
+		spacer =			(array.length > 1) ? " " : ""
+		and_string =	(array.length > 1 && index == array.length - 1) ? "and" : ""
+		output_string += (index == array.length - 1) ? "#{and_string}#{spacer}#{item}" : "#{item}#{comma}#{spacer}"
+	end
+	output_string
 end

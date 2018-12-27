f96edadb8c9046e689bbd584cf86a63074ed4d3e
mooting <justin_w_davis@yahoo.com>
2018-12-26 23:14:38 -0800

    solved

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    return array.first
+  when 2
+    return array.join(' and ')
+  else
+    array << "and #{array.pop}"
+    return array.join(', ')
+  end
 end

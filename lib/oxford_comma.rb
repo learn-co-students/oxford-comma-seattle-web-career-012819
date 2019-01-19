8b0468c53960183eab44062484864611e460c038
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 01:23:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    "#{array.shift} and #{array.shift}"
+  elsif array.size == 3
+    "#{array.shift}, #{array.shift}, and #{array.shift}"
+  elsif array.size > 3
+    counter = 1
 
+    string = "#{array.shift}"
+
+    for counter in 1..array.size-1 do
+      string.concat(",")
+      string.concat(" #{array.shift}")
+    end
+
+    string.concat(", and #{array.shift}")
+
+  end
 end

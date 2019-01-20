ecf40dea42db4d19ab1b88bd1a835713c0e7b08f
yong liang <yongliang24@gmail.com>
2019-01-20 05:19:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last_element = "and #{array[-1]}"
+    array.pop
+    array.push(last_element)
+    return array.join(", ")
+  end
 end

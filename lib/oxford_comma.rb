3d961b2eb6296eebe75730603cb56b076b7e6b2a
Amy Cohen <amy.cohen@flatironschool.com>
2019-04-11 15:44:03 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        array.insert(array.length-1, "and")
+        str = array.join(", ")
+        comma_index = str.index("and,")
+        str.sub("and,", "and")
+    end
 
 end
+
+oxford_comma(["kiwi"])
+oxford_comma(["kiwi", "durian"])
+oxford_comma(["kiwi", "durian", "starfruit"])
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
